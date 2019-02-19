0d0b26013de9cd826ba17d282e1f9b53e7747008
Vladimir Beliy <eastgym@ua.fm>
2019-02-19 13:06:48 +0200

    Moved exercises and workouts to JSON

--- script/app.js
+++ script/app.js
@@ -1,21 +1,9 @@
 var audio = new Audio('assets/sound/success.wav');
-var exerciseData = JSON.parse(data);
+var exercisesData = JSON.parse(exercises);
+var workoutsData = JSON.parse(workouts);
 
 function sleep(sec) {
     return new Promise(resolve => setTimeout(resolve, sec * 1000));
-}
-
-function runExercise(exerciseId) {
-    audio.play();
-    for (var i in exerciseData) {
-        if (exerciseData[i].id === exerciseId) {
-            document.getElementById("exBig").innerHTML = exerciseData[i].title;
-            document.getElementById("exDetailedDesc").innerHTML = exerciseData[i].details;
-            document.getElementById("exImg").innerHTML = `<img src='assets/img/${exerciseData[i].image}' />`;
-            document.getElementById("countdown").innerHTML = `${exerciseData[i].duration} seconds`;
-            break;
-        }
-    }
 }
 
 function workoutIsOver() {
@@ -26,39 +14,33 @@ function workoutIsOver() {
     document.getElementById("countdown").innerHTML = "";
 }
 
+function getWorkoutId() {
+    const urlParams = new URLSearchParams(window.location.search);
+    return urlParams.get('workoutId');
+}
+
 async function runEyesExercises() {
-    runExercise("restEyesShort");
-    await sleep(10);
-    runExercise("upDown");
-    await sleep(20);
-    runExercise("restEyesShort");
-    await sleep(10);
-    runExercise("leftRight");
-    await sleep(20);
-    runExercise("goDiagonal");
-    await sleep(20);
-    runExercise("restEyesShort");
-    await sleep(10);
-    runExercise("clockwise");
-    await sleep(10);
-    runExercise("oppositeClockwise");
-    await sleep(10);
-    runExercise("spiral");
-    await sleep(20);
-    runExercise("restEyesShort");
-    await sleep(10);
-    runExercise("distance");
-    await sleep(20);
-    runExercise("restEyesShort");
-    await sleep(10);
+    let workoutId = getWorkoutId();
+    for (var i in workoutsData) {
+        if (workoutId === workoutsData[i].id) {
+            let exercisesArr = workoutsData[i].exercises;
+            for (var j in exercisesArr) {
+                for (var i in exercisesData) {
+                    if (exercisesData[i].id === exercisesArr[j]) {
+                        audio.play();
+                        document.getElementById("exBig").innerHTML = exercisesData[i].title;
+                        document.getElementById("exDetailedDesc").innerHTML = exercisesData[i].details;
+                        document.getElementById("exImg").innerHTML = `<img src='assets/img/${exercisesData[i].image}' />`;
+                        document.getElementById("countdown").innerHTML = `${exercisesData[i].duration} seconds`;
+                        await sleep(exercisesData[i].duration);
+                        break;
+                    }
+                }
+            }
+            break;
+        }
+    }
     workoutIsOver();
 }
 
-function getUrlParameters() {
-    const urlParams = new URLSearchParams(window.location.search);
-    const workoutIdParam = urlParams.get('workoutId');
-    console.log('My param is: ' + workoutIdParam);
-}
-
-//getUrlParameters();
 runEyesExercises();
